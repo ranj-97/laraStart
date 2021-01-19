281981f73dcc322d3e6f77fb09908eaf63fd6da9
Your Name <you@example.com>
2021-01-19 12:59:00 +0300

    route and making html and sass better

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,23 @@ require('admin-lte');
 
 window.Vue = require('vue').default;
 
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+let routes = [
+    { path:'/dashboard', component:  require('./components/Dashboard.vue').default},
+    { path:'/profile', component: require('./components/Profile.vue').default}
+  ]
+
+  let router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+  })
+
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -23,7 +40,7 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -33,4 +50,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,25 +9,24 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>AdminLTE 3 | Starter</title>
+
+    <title>Dashboard</title>
 
 
-    {{-- script --}}
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    
     {{-- css --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- script --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/adminlte.min.css">
-    <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+
 </head>
 
 <body class="hold-transition sidebar-mini">
-    <div class="wrapper">
+    <div id="app" class="wrapper" >
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -179,20 +178,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
 
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                {{-- <i class="nav-icon fas fa-th"></i>
-                                --}}
+                            <router-link to="/dashboard" class="nav-link">
+                                
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
                                     Dashboard
                                     {{-- <span class="right badge badge-danger">New</span>
                                     --}}
                                 </p>
-                            </a>
+                              </router-link>
                         </li>
 
                         <li class="nav-item has-treeview">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-cog"></i>
                                 <p>
                                     Management
@@ -201,7 +199,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link active">
+                                    <a href="#" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Active Page</p>
                                     </a>
@@ -216,14 +214,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <router-link to="/profile" class="nav-link">
                                 {{-- <i class="nav-icon fas fa-th"></i>
                                 --}}
                                 <i class="nav-icon fas fa-user"></i>
                                 <p>
                                     Profile
                                 </p>
-                            </a>
+                              </router-link>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
@@ -244,13 +242,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-          
+
 
             <!-- Main content -->
             <div class="content">
                 <div class="container-fluid">
+                    <router-view>
+
+                    </router-view>
                     
-                  
+
+
                 </div><!-- /.container-fluid -->
             </div>
             <!-- /.content -->
@@ -279,15 +281,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </footer>
     </div>
     <!-- ./wrapper -->
-
-    <!-- REQUIRED SCRIPTS -->
-
-    <!-- jQuery -->
-    <script src="plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
